6314cb6bb7fde5e9cc4b7ee22f4ececf57723763
UnalloyedMe <gahyeji@proton.me>
2026-05-23 04:22:08 +0200

    Babystep 3

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # KronOS
 The goal is to make a OS based on reversing, just as Kali is based on hacking.
+## To emulate the bootloader :
+### With emulation :
+```bash
+nasm boot.asm -f bin -o boot.bin
+qemu-system-i386 -hda boot.bin
+```
--- boot.asm
+++ boot.asm
@@ -1,5 +1,22 @@
-	cli
-hang:
-	jmp hang
+; boot.asm
+   mov ax, 0x07c0
+   mov ds, ax ;puts Data Segment at 0x07c0 (standard bootloader adress)
 
-	times 512-($-$$) db 0
+   mov si, msg 
+   cld
+loop:
+   lodsb ;loads si into al
+   or al, al ; zero=end of str
+   jz hang   ; get out
+   mov ah, 0x0E ;display function from BIOS video mode
+   mov bh, 0
+   int 0x10 ;BIOS interrupt video 
+   jmp loop
+
+hang:
+   jmp hang
+
+msg   db 'Hello World', 13, 10, 0 ;wihtout the specifications at the end, the bootloader wouldn't know when to stop
+   times 510-($-$$) db 0
+   db 0x55
+   db 0xAA
